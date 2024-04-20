13867bd4fa0149a030ed8e17688342df42151645
Alexander J. Gray <alexanderJuliusGray@gmail.com>
2024-04-20 18:05:36 -0400

    Various syntax and code format cleanup.

--- js/rootUrl.js
+++ js/rootUrl.js
@@ -1,11 +1,10 @@
 // Get base URL of page
 var rootUrl = window.location.origin;
-console.log(rootUrl)
+
 // Get all elements with CLASS rootLink
 var elements = document.querySelectorAll('.rootLink')
-console.log(rootUrl)
+
 // Iterate through each element replace href with rootUrl
 elements.forEach(function(element) {
     element.setAttribute('href', rootUrl);
-    console.log(rootUrl)
 });  
